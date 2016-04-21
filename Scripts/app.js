b7ba799d6ecba3dd2dd0ba38dabeb342422b58c8
brad-langshaw <brad.langshaw@live.ca>
2016-04-21 11:26:04 -0400

    added numbers

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,18 +12,23 @@
       var dice1 = document.getElementById("dice1");
       var dice2 = document.getElementById("dice2");
       var button = document.getElementById("button");
-      
+      var number1 = document.getElementById("number1");
+      var number2 = document.getElementById("number2");
       
       button.addEventListener("click", function buttonClick(){
-            dice1.innerHTML = (("<img src='../Asset/images/dice") + [rollDice] + (".png' title='dice'/>"));
-            dice2.innerHTML = (("<img src='../Asset/images/dice") + [rollDice] + (".png' title='dice'/>"));
+            rollDice;
+            number1.innerHTML = [rolledDice]
+            dice1.innerHTML = (("<img src='../Asset/images/dice") + [rolledDice] + (".png' title='dice'/>"));
+            rollDice;
+            number2.innerHTML = [rolledDice]
+            dice2.innerHTML = (("<img src='../Asset/images/dice") + [rolledDice] + (".png' title='dice'/>"));
             
    
       });
     
     
     function rollDice(){
-               math.floor((math.random() * 6) +1);
+              var rolledDice = math.floor((math.random() * 6) +1);
              };
              
          
