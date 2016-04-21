78c219af20a0ca719a0a8066ad4bd7ea7d36afd4
brad-langshaw <brad.langshaw@live.ca>
2016-04-21 10:45:37 -0400

    more fixes

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,18 +7,18 @@
 
 
     var button = null;
-    var dice = ["../Asset/images/dice1.png", "../Asset/images/dice2.png", "../Asset/images/dice3.png", "../Asset/images/dice4.png", "../Asset/images/dice5.png", "../Asset/images/dice6.png"]
+    var dice = ["../Asset/images/dice1.png", "../Asset/images/dice2.png", "../Asset/images/dice3.png", "../Asset/images/dice4.png", "../Asset/images/dice5.png", "../Asset/images/dice6.png"];
     //CreateJS Section ++++++++++++++++++++++++++++++++++++
 
     //global variables
-      var dice1 = document.getElementById("dice1")
-      var dice2 = document.getElementById("dice2")
-      var button = document.getElementById("button")
+      var dice1 = document.getElementById("dice1");
+      var dice2 = document.getElementById("dice2");
+      var button = document.getElementById("button");
       
       
       button.on("click", function(){
-            dice1.innerHTML(dice[rollDice])
-            dice2.innerHTML(dice[rollDice])
+            dice1.innerHTML(dice[rollDice]);
+            dice2.innerHTML(dice[rollDice]);
             
    
       });
